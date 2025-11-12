01364f0552bf0e4117bb993cad37c5c070551b05
omarwael16004383 <omar.hamdy@student.giu-uni.de>
2025-11-12 15:07:21 +0200

    Update Enclosure.h
    i uploaded enclosure

--- Enclosure.h
+++ Enclosure.h
@@ -1,15 +1,29 @@
-#ifndef ENCLOSURE_H
-#define ENCLOSURE_H
+#ifndef ENCLOSURE_H_INCLUDED
+#define ENCLOSURE_H_INCLUDED
+#include <string>
+#include<iostream>
+using namespace std;
+class Enclosure{
+private:
+    Animal*animal;
+    int capacity;
+    int currentCount;
+public:
+    Enclosure();
+    Enclosure(Animal a,int capacity,int currentCount);
+    void addAnimal(Animal*a);
+    void displayAnimal();
+    Animal getanimal();
+    int getCapacity();
+    int getCount();
+    void setCapacity();
+    void setAnimal();
+    void setCount();
+    ~Enclosure();
 
-#include "Animal.h"
 
-//define enclosure class here 
-
+};
 
 
 
-
-
-
-
-#endif
+#endif // ENCLOSURE_H_INCLUDED

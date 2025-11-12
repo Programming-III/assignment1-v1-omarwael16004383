ac24a5f97690df0505f26a530f336629aff250bb
omarwael16004383 <omar.hamdy@student.giu-uni.de>
2025-11-12 15:05:48 +0200

    Update Animal.h
    i uploaded animal

--- Animal.h
+++ Animal.h
@@ -1,11 +1,34 @@
-#ifndef ANIMAL_H
-#define ANIMAL_H
-
+#ifndef ANIMAL_H_INCLUDED
+#define ANIMAL_H_INCLUDED
 #include <string>
+#include<iostream>
 using namespace std;
+class Animal{
+     private:
+         string name;
+         int age;
+         bool isHungry;
+     public:
+         Animal(string name,int age,bool isHungry);
+         Animal();
+         void display();
+         void feed();
+         string getName();
+         int getAge();
+         bool getisHungry();
+         void setName();
+         void setAge();
+         void setIsHungry();
+         ~Animal();
 
-//define animal class here 
 
 
 
-#endif
+
+
+
+
+};
+
+
+#endif // ANIMAL_H_INCLUDED
